63ad207b43128e035fdb0839dad72aae87d298ae
soupsueapple <soupsueapple@gmail.com>
2017-04-12 10:51:54 +0800

    Initial Commit

---
+++ SoupReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoupReader
+//
+//  Created by 汤坤 on 2017/4/12.
+//  Copyright © 2017年 汤坤. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
